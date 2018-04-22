98d8c0dfbdfaf3c886ec70ad3e90858fa91f0c0f
thomasantonakis <thomas.antonakis@gmail.com>
2018-04-22 23:28:10 +0300

    Update libraries in a separate file

---
+++ libraries.R
@@ -0,0 +1,13 @@
+# Load required libraries
+
+# load library for google Authentiction
+library(googleAuthR)
+
+# Load library for manipulating data from Google Analytics
+library(googleAnalyticsR)
+
+# Load library for retrieving data from the Webmasters console
+library(searchConsoleR)
+
+# load library for communication with BigQuery
+library(bigrquery)
