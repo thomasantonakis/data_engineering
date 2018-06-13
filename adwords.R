73420d380a647458b3117790fb84b35530d72627
thomasantonakis <thomas.antonakis@gmail.com>
2018-06-13 20:44:31 +0300

    createion of two reports and comments on next steps

--- adwords.R
+++ adwords.R
@@ -1,6 +1,8 @@
-# reports()
+reports()
 metrics(report='CAMPAIGN_PERFORMANCE_REPORT')
 metrics(report='ADGROUP_PERFORMANCE_REPORT')
+metrics(report = 'KEYWORDS_PERFORMANCE_REPORT')
+metrics(report = 'SEARCH_QUERY_PERFORMANCE_REPORT')
 
 yesterday <- gsub("-","",format(Sys.Date()-1,"%Y-%m-%d"))
 thirtydays<- gsub("-","",format(Sys.Date()-29,"%Y-%m-%d"))
@@ -8,31 +10,37 @@ thirtydays<- gsub("-","",format(Sys.Date()-29,"%Y-%m-%d"))
 
 
 # Create statement
-#Example 1
-# body <- statement(select=c('Clicks','Impressions','Cost','AverageCpc'),
-#                   report="ACCOUNT_PERFORMANCE_REPORT",
-#                   start="2018-05-01",
-#                   end="2018-06-01")
-#Example 2
-body <- statement(select=c('CampaignName','CampaignType', 'Device',
-                           'Clicks','Impressions','Cost','AverageCpc', 'AveragePosition', 'Conversions', 'ConversionRate',
-                           'SearchBudgetLostImpressionShare', 'SearchRankLostImpressionShare' ),
-                  report="CAMPAIGN_PERFORMANCE_REPORT",
+# adwords_keywords
+body1 <- statement(select=c('CampaignName','AdGroupName', 'QualityScore', 'Device', 'Date','Criteria', 'Id','TopOfPageCpc',
+                           'AdNetworkType2','Clicks','Impressions','Cost','AveragePosition', 'Conversions',
+                           'SearchImpressionShare', 'SearchRankLostImpressionShare' ),
+                  report="KEYWORDS_PERFORMANCE_REPORT",
+                  start="2018-05-01",
+                  end="2018-06-01")
+
+# adwords_search_term
+body2 <- statement(select=c('CampaignName','AdGroupName', 'Device', 'Date','Query', 'QueryMatchTypeWithVariant',
+                            'KeywordId',
+                           'Clicks','Impressions','Cost','AverageCpc', 'AveragePosition', 'Conversions', 'ConversionRate'),
+                  report="SEARCH_QUERY_PERFORMANCE_REPORT",
                   # where="CampaignName STARTS_WITH 'A' AND Clicks > 100",
                   start="2018-05-01",
                   end="2018-05-31")
-# #Example 3
-# body <- statement(select=c('Criteria','Clicks','Cost','Ctr'),
-#                   report="KEYWORDS_PERFORMANCE_REPORT",
-#                   where="Clicks > 100",
-#                   start="2018-05-01",
-#                   end="2018-06-01")                     
+
+# What is needed is the map of ID and Keywords from the one report so that we can join them to the seconf report
+# Just search the report name to the adwords api docs so that we find out the proper names of the columns needed
+# Manipulation is needed afterqwards for formatting reasons
+# Check the Top Of Page CPC format, seems a bit weird
   
 # Pull data from Adwords
 # Check with UO
-adwordsData <- getData(clientCustomerId=adwordsCustomerID, google_auth=google_auth, statement=body)
+adwords_keywords <- getData(clientCustomerId=adwordsCustomerID, google_auth=google_auth, statement=body1)
+adwords_search_term <- getData(clientCustomerId=adwordsCustomerID, google_auth=google_auth, statement=body2)
 
 # Alex says that we need :
 # Adgroup Performance without getting the conversion name and type
 # Adgroup performance report with conversions with all the breakdowns
 # Label report me labelID
+
+
+# Upload to BQ etc
